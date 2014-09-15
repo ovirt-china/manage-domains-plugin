699a3789d000d0138312db22cc2b6ec144aae31d
Lucas Vandroux <lucas.vandroux@gmail.com>
2014-09-15 18:15:57 +0800

    Create different folders for UIPlugin and Servlet

---
+++ Servlet/PluginEMDServlet.java
@@ -0,0 +1,19 @@
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*;
+
+public class HelloWorldServlet extends HttpServlet {
+  public void service(HttpServletRequest req,
+                      HttpServletResponse res)
+       throws IOException
+  {
+    // Must set the content type first
+    res.setContentType("text/html");
+    // Now obtain a PrintWriter to insert HTML into
+    PrintWriter out = res.getWriter();
+
+    out.println("<html><head><title>" +
+                "Hello World!</title></head>");
+    out.println("<body><h1>Hello World!</h1></body></html>");
+  }
+}
