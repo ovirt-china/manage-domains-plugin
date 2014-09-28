6e644b59100388e3582f864714e66460b720749c
Lucas Vandroux <lucas.vandroux@gmail.com>
2014-09-28 17:34:43 +0800

    Js file for the Add Domain Dialog

---
+++ UIPlugin/emdplugin-resources/js/add.js
@@ -0,0 +1,33 @@
+'use strict';
+
+// Use in the modal to add a new domain
+
+(function() {
+
+   var app = angular.module('plugin.add', ['plugin.common']);
+
+   app.factory('dialogManager', ['pluginApi', 'urlUtil', function (pluginApi, urlUtil) {
+      return {
+         showDialog: function () {
+            pluginApi.showDialog('Add Domain', 'add-domain-dialog', urlUtil.relativeUrl('add.html'), '640px', '480px',
+            {
+              buttons: [
+                  {
+                     label: 'Close',
+                     onClick: function() {
+                           pluginApi.closeDialog('add-domain-dialog');
+                     }
+                  }
+              ],
+              resizeEnabled: true,
+              closeIconVisible: true,
+              closeOnEscKey: true
+           });
+         }
+      };
+   }]);
+
+   
+
+
+})();
