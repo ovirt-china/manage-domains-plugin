0de097c453f0815a43b852b465a95939b22b47d8
Lucas Vandroux <lucas.vandroux@gmail.com>
2014-09-29 19:07:28 +0800

    Remove the useless code.

--- UIPlugin/emdplugin-resources/js/add.js
+++ UIPlugin/emdplugin-resources/js/add.js
@@ -6,37 +6,13 @@
 
    var app = angular.module('plugin.add', ['plugin.common']);
 
-   app.factory('dialogManager', ['pluginApi', 'urlUtil', function (pluginApi, urlUtil) {
-      return {
-         showDialog: function () {
-            pluginApi.showDialog('Add Domain', 'add-domain-dialog', urlUtil.relativeUrl('add.html'), '640px', '480px',
-            {
-              buttons: [
-                  {
-                     label: 'Close',
-                     onClick: function() {
-                           pluginApi.closeDialog('add-domain-dialog');
-                     }
-                  }
-              ],
-              resizeEnabled: true,
-              closeIconVisible: true,
-              closeOnEscKey: true
-           });
-         }
-      };
-   }]);
-
    // Allow the chevron to change state when the Advanced Parameters are collapsed or not.
    app.controller('collapseWatcher', ['$scope', function($scope) {
-     $scope.showAdvParam = false;
+      $scope.showAdvParam = false;
 
       $scope.actCollapse = function(state) {
-        $scope.showAdvParam = !state;
-    };
-  }]);
-
-
-
-
+         $scope.showAdvParam = !state;
+      };
+   }]);
+   
 })();
