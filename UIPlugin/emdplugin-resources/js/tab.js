828e7a0cc606a3feb359a90143f485ae0a17bc25
Lucas Vandroux <lucas.vandroux@gmail.com>
2014-09-29 18:56:39 +0800

    Replace the code at its own place.

--- UIPlugin/emdplugin-resources/js/tab.js
+++ UIPlugin/emdplugin-resources/js/tab.js
@@ -6,71 +6,45 @@
 
    var app = angular.module('plugin.tab', ['plugin.common']);
 
-   // Get API object for 'domain-name-mgmt' plugin
-   app.factory('pluginApi', ['$window', 'pluginName', function ($window, pluginName) {
-      return $window.parent.pluginApi(pluginName);
-   }]);
+   app.factory('dataManager', ['$window', '$rootScope', function ($window, $rootScope) {
+      var treeItemType, treeItemEntityId;
 
-   app.factory('tabManager', ['pluginApi', 'urlUtil', function (pluginApi, urlUtil) {
-      var tabWindow, selectedTreeItem;
       return {
-         addTab: function () {
-            pluginApi.addMainTab('Domains', 'emd-tab', urlUtil.relativeUrl('emd-tab.html'));
-         },
-         setTabWindow: function (window) {
-            tabWindow = window;
-         },
-         setSelectedTreeItem: function (item) {
-            selectedTreeItem = item;
-         },
-         updateTab: function () {
-            if (tabWindow && selectedTreeItem) {
-               var type = selectedTreeItem.type;
-               var entityId = selectedTreeItem.entity && selectedTreeItem.entity.id;
-               tabWindow.setTestData(type, entityId);
-            }
-         }
-      };
-   }]);
-
-   // Define event handler functions for later invocation by UI plugin infrastructure
-   app.factory('pluginEventHandlers', ['pluginName', 'tabManager', function (pluginName, tabManager) {
-      return {
-         UiInit: function () {
-            tabManager.addTab();
-         },
-         MessageReceived: function (dataString, sourceWindow) {
-            var data = JSON.parse(dataString);
-            if (data && data.sender === pluginName) {
-               if (data.action === 'GetTabData') {
-                  tabManager.setTabWindow(sourceWindow);
-                  tabManager.updateTab();
-               }
-            }
-         },
-         SystemTreeSelectionChange: function (selectedItem) {
-            tabManager.setSelectedTreeItem(selectedItem);
-            tabManager.updateTab();
-         }
-      };
-   }]);
-
-   // Register event handler functions and tell the API we are good to go.
-   app.factory('initService', ['pluginApi', 'pluginEventHandlers', function (pluginApi, pluginEventHandlers) {
-      return {
-         bootstrapPlugin: function () {
-            var apiOptions = {
-               allowedMessageOrigins: ['http://localhost:8080']
+         exposeTestDataFunction: function () {
+            $window.setTestData = function (type, entityId) {
+               treeItemType = type;
+               treeItemEntityId = entityId;
+               $rootScope.$apply();
             };
-            pluginApi.options(apiOptions);
-            pluginApi.register(pluginEventHandlers);
-            pluginApi.ready();
+         },
+         getTreeItemType: function () {
+            return treeItemType || '(no type)';
+         },
+         getTreeItemEntityId: function () {
+            return treeItemEntityId || '(no entity)';
          }
       };
    }]);
 
-   app.run(['initService', function (initService) {
-      initService.bootstrapPlugin();
+   app.controller('tabController', ['$scope', 'dataManager', function ($scope, dataManager) {
+      $scope.$watch(function() {
+         return dataManager.getTreeItemType();
+      },
+
+      function(newVal, oldVal) {
+         $scope.treeItemType = newVal;
+      });
+
+      $scope.$watch(function() {
+         return dataManager.getTreeItemEntityId();
+      }, function(newVal, oldVal) {
+         $scope.treeItemEntityId = newVal;
+      });
+   }]);
+
+   app.run(['messageUtil', 'dataManager', function (messageUtil, dataManager) {
+      dataManager.exposeTestDataFunction();
+      messageUtil.sendMessageToParent('GetTabData');
    }]);
 
    app.controller('dialogController', ['pluginApi', 'urlUtil', function (pluginApi, urlUtil) {
