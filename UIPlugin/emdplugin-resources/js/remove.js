6816854d207bd74e9be43b188b3a6ce15f7359ae
Lucas Vandroux <lucas.vandroux@gmail.com>
2014-10-11 12:19:19 +0800

    Still debugging

--- UIPlugin/emdplugin-resources/js/remove.js
+++ UIPlugin/emdplugin-resources/js/remove.js
@@ -7,15 +7,9 @@
    var app = angular.module('plugin.remove', ['plugin.common']);
 
    app.controller('CacheController', ['dialogCache', '$scope', function(cache, $scope){
-
-    //  $scope.domain = cache.get('domainToRemove');
-
-    $scope.domain = function(){
-      var namefromcache = cache.get('domainToRemove');
-      console.log('In the cache:' + namefromcache );
-
-      return cache.get('domainToRemove');
-    }
+    var namefromcache = cache.get('domainToRemove');
+    $scope.domain = namefromcache;
+  
    }]);
 
 })();
