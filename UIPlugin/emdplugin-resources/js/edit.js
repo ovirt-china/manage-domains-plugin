d5b1d6d38f89f31cb26b2815955c2907210597ff
Lucas Vandroux <lucas.vandroux@gmail.com>
2014-10-16 12:17:01 +0800

    Implement the verification and submission of the form.

--- UIPlugin/emdplugin-resources/js/edit.js
+++ UIPlugin/emdplugin-resources/js/edit.js
@@ -4,13 +4,51 @@
 
 (function() {
 
-   var app = angular.module('plugin.edit', ['plugin.common']);
+  var app = angular.module('plugin.edit', ['plugin.common']);
 
-   // Get the information about the domain from the local storage
-   app.controller('CacheController', ['cacheService', '$scope', function(cache, $scope){
+  app.value('dialogName', 'edit-dialog');
+
+  app.run(['messager', 'dialogName', function (messager, dialogName) {
+
+    messager.sendActionMessage(dialogName, 'justOpen', null);
+  }]);
+
+   app.controller('EditFormController',['$scope', '$window', 'messager', 'dialogName', function($scope, $window, messager, dialogName){
+
+     $scope.submit = function() {
+        // First verify the form
+        if($scope.addForm.$valid){
+          console.log('[EMDPlugin > edit.js > EditFormController]' + '\n' + '--> The form is valid.');
+
+          // Test if the domain object is define
+          if($scope.domain){
+            $scope.domainJSON = angular.toJson($scope.domain);
+            console.log('[EMDPlugin > edit.js > EditFormController]' + '\n' + '--> Information from the form ' + angular.toJson($scope.domain));
+          }
+
+          //////////////////////////////////////////////////////////////////////
+          //                                                                  //
+          //                  SEND THE REQUEST TO THE API                     //
+          //                                                                  //
+          //////////////////////////////////////////////////////////////////////
+
+          // Close the window is evrything went well.
+          messager.sendActionMessage(dialogName, 'close', dialogName);
+
+        } else {
+           $window.alert("Your form is not correct ! (Sorry, I don't know what to say to help you :( )");
+        }
+
+      };
+   }]);
+
+
+
+  // Get the information about the domain from the local storage
+  app.controller('CacheController', ['cacheService', '$scope', function(cache, $scope){
 
     $scope.domain = cache.getData('domainToEdit');
 
-   }]);
+  }]);
 
 })();
