a392d593cc5f17f71f8f13c3949adf7f88d38c85
Lucas Vandroux <lucas.vandroux@gmail.com>
2014-10-17 16:31:25 +0800

    Update the repository with remaining files.

--- Servlet/ServletUIPluginEMD/src/main/java/com/servlet/uiplugin/emd/HelloWorld.java
+++ Servlet/ServletUIPluginEMD/src/main/java/com/servlet/uiplugin/emd/HelloWorld.java
@@ -1,18 +1,33 @@
 package com.servlet.uiplugin.emd;
 import javax.servlet.*;
 import javax.servlet.http.*;
+
 import java.io.*;
 
 public class HelloWorld {
+	private String defaultName = "Bernard";
+	private String defaultGreeting = "你好";
+	
 	public void service(HttpServletRequest req, HttpServletResponse res)throws IOException
 	{
-		// Must set the content type first
-		res.setContentType("text/html");
-		// Now obtain a PrintWriter to insert HTML into
-		PrintWriter out = res.getWriter();
-		
-		out.println("<html><head><title>" +
-		        "Hello World!</title></head>");
-		out.println("<body><h1>Hello World!</h1></body></html>");
+		String name, paramName[];
+		  if ((paramName = req.getParameterValues("name")) 
+		      != null) {
+		    name = paramName[0];
+		  }
+		  else {
+		    name = defaultName;
+		  }
+
+		  // Set the content type first
+		  res.setContentType("text/html");
+		  // Obtain a PrintWriter as an output stream
+		  PrintWriter out = res.getWriter();
+
+		  out.print("<html><head><title>" + 
+		              "Hello World!" + "</title></head>");
+		  out.print("<body><h1>");
+		  out.print(defaultGreeting + " " + name + "!");
+		  out.print("</h1></body></html>");
 	}
 }
