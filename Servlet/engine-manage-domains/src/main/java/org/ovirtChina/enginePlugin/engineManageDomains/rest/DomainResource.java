47b55bf6b771a6af0cfc17ea97c9dbd7b8f659d6
Lucas Vandroux <lucas.vandroux@gmail.com>
2014-10-24 15:59:02 +0800

    add a Post function to add a domain.

--- Servlet/engine-manage-domains/src/main/java/org/ovirtChina/enginePlugin/engineManageDomains/rest/DomainResource.java
+++ Servlet/engine-manage-domains/src/main/java/org/ovirtChina/enginePlugin/engineManageDomains/rest/DomainResource.java
@@ -4,13 +4,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
 
 import org.ovirtChina.enginePlugin.engineManageDomains.model.Domain;
 
-@Path("/domain")
+@Path("/domains")
 public class DomainResource {
 
   @GET
@@ -25,5 +29,15 @@ public class DomainResource {
     }
     return domainList;
   }
+
+  @POST
+  @Path("/add")
+  @Consumes(MediaType.APPLICATION_JSON)
+  public Response consumeJSON ( Domain domain ) {
+
+    return Response.status(200).entity(domain).build();
+  }
+
+
 
 }
