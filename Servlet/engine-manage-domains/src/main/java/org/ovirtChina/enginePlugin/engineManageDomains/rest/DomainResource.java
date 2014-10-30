9cb9fe3eee5a17e9f15caf118aa234771f3ff93c
Lucas Vandroux <lucas.vandroux@gmail.com>
2014-10-30 17:37:25 +0800

    Add a DELETE function to remove domains.

--- Servlet/engine-manage-domains/src/main/java/org/ovirtChina/enginePlugin/engineManageDomains/rest/DomainResource.java
+++ Servlet/engine-manage-domains/src/main/java/org/ovirtChina/enginePlugin/engineManageDomains/rest/DomainResource.java
@@ -5,7 +5,9 @@ import java.util.List;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.core.MediaType;
@@ -22,24 +24,44 @@ public class DomainResource {
   @Produces(MediaType.APPLICATION_JSON)
   public List<Domain> printDomain() {
     List<Domain> domainList = new ArrayList<Domain>();
-    String[] names = {"AD_DOMAIN","auth-server"};
+    String[] domains = {"AD_DOMAIN","auth-server"};
     String username = "admin";
-    for(String name:names){
-      domainList.add(new Domain(name,username));
+    for(String domain:domains){
+      domainList.add(new Domain(domain,username));
     }
     return domainList;
+    //return Response.status(200).setContentType("application/json").
   }
 
   @POST
   @Path("/add")
   @Consumes(MediaType.APPLICATION_JSON)
-  public Response consumeJSON ( Domain domain ) {
+  public Response addDomain( Domain domain ) {
 
     String output = domain.toString();
 
     return Response.status(200).entity(output).build();
   }
 
+  @DELETE
+  @Path("/{domain}/remove")
+  public Response removeDomain(@PathParam("domain") String domain) {
 
+    //Test purpose
+    String testName = "domain_test";
+
+    if(domain == testName){
+
+      String output = "The domain " + domain + " has been removed correctly.";
+
+      return Response.status(204).entity(output).build();
+
+    }else{
+
+      String output = "Impossible to remove the domain " + domain + ". This domain doesn't exist.";
+
+      return Response.status(404).entity(output).build();
+    }
+  }
 
 }
