88c9693b37bcb3de8f750b22c5fd1bc49b1bb1f0
Lucas Vandroux <lucas.vandroux@gmail.com>
2014-11-06 15:50:52 +0800

    Remove status in the domain list for the first version.

--- Servlet/engine-manage-domains/src/main/java/org/ovirtChina/enginePlugin/engineManageDomains/model/Domain.java
+++ Servlet/engine-manage-domains/src/main/java/org/ovirtChina/enginePlugin/engineManageDomains/model/Domain.java
@@ -4,22 +4,14 @@ public class Domain {
 
   private String domain;
   private String username;
-  private Boolean status;
 
   public Domain(){
     this.domain="";
     this.username="";
-    this.status=false;
   }
   public Domain(String domain,String username){
     this.domain=domain;
     this.username=username;
-    this.status=false;
-  }
-  public Domain(String domain,String username, Boolean status){
-    this.domain=domain;
-    this.username=username;
-    this.status=status;
   }
   public String getDomain() {
     return domain;
@@ -27,15 +19,11 @@ public class Domain {
   public String getUsername() {
     return username;
   }
-  public Boolean getStatus() {
-    return status;
-  }
 
   @Override
 	public String toString() {
 		return new StringBuffer(" Domain : ").append(this.domain)
-				.append(" Username : ").append(this.username)
-        .append(" Status : ").append(this.status).toString();
+				.append(" Username : ").append(this.username).toString();
 	}
 
 }
