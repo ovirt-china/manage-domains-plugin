cf1c3fcafe4b7340d04e6f8fe971b8cad1ef1387
Lucas Vandroux <lucas.vandroux@gmail.com>
2014-11-27 16:32:46 +0800

    Testing new organization.

--- UIPlugin/emdplugin-resources/js/ajax.js
+++ UIPlugin/emdplugin-resources/js/ajax.js
@@ -12,11 +12,10 @@
 
   app.factory('request',['$http', 'URL', 'sourceName', 'messager', 'msgRemoveUsers', 'msgRestartEngine', 'translationService', function($http, URL, sourceName, messager, msgRemoveUsers, msgRestartEngine, translationService){
 
-    var tr = translationService.getTranslationJSON('zh');
-
     return {
 
       list: function(){
+        var tr = translationService.getTranslationJSON('zh');
         var urlReq = URL + '/domains/';
         console.log('API Request: GET - ' + urlReq);
         $http.get(urlReq).
@@ -98,49 +97,51 @@
   }]);
 
   app.factory('translateService', function(){
-    var english = {"NOTIFICATION_WELCOME" : "Thanks for using this plugin. All the code is on <a href=\"https://github.com/eayun/UIPlugin-Engine-Manage-Domains\"> GitHub</a>. If you have any suggestion please <a href=\"https://github.com/eayun/UIPlugin-Engine-Manage-Domains/issues\">open an issue on GitHub</a>.",
-    "NOTIFICATION_NEED_RESTART" : "oVirt Engine restart is required in order for the changes to take place (service ovirt-engine restart).",
-    "NOTIFICATION_REMOVE_USERS" : "Please remove all users and groups of this domain using the Administration portal or the API.",
+    var english = {
+      NOTIFICATION_WELCOME : "Thanks for using this plugin. All the code is on <a href=\"https://github.com/eayun/UIPlugin-Engine-Manage-Domains\"> GitHub</a>. If you have any suggestion please <a href=\"https://github.com/eayun/UIPlugin-Engine-Manage-Domains/issues\">open an issue on GitHub</a>.",
+    NOTIFICATION_NEED_RESTART : "oVirt Engine restart is required in order for the changes to take place (service ovirt-engine restart).",
+    NOTIFICATION_REMOVE_USERS : "Please remove all users and groups of this domain using the Administration portal or the API.",
 
-    "NOTIFICATION_REFRESH_FAILED" : "Impossible to refresh the list of Domains.",
-    "NOTIFICATION_REFRESH_SUCCESS" : "The list of Domains has been refreshed successfully.",
+    NOTIFICATION_REFRESH_FAILED : "Impossible to refresh the list of Domains.",
+    NOTIFICATION_REFRESH_SUCCESS : "The list of Domains has been refreshed successfully.",
 
-    "NOTIFICATION_DELETE_SUCCESS_1" : "<strong>",
-    "NOTIFICATION_DELETE_SUCCESS_2" : "</strong> has been successfully deleted.",
-    "NOTIFICATION_DELETE_FAILED_1" : "Impossible to delete the domain <strong>",
-    "NOTIFICATION_DELETE_FAILED_2" : "</strong>.",
+    NOTIFICATION_DELETE_SUCCESS_1 : "<strong>",
+    NOTIFICATION_DELETE_SUCCESS_2 : "</strong> has been successfully deleted.",
+    NOTIFICATION_DELETE_FAILED_1 : "Impossible to delete the domain <strong>",
+    NOTIFICATION_DELETE_FAILED_2 : "</strong>.",
 
-    "NOTIFICATION_ADD_SUCCESS_1" : "The domain <strong>",
-    "NOTIFICATION_ADD_SUCCESS_2" : "</strong> has been added successfully.",
-    "NOTIFICATION_ADD_FAILED_1" : "Impossible to add the domain <strong>",
-    "NOTIFICATION_ADD_FAILED_2" : "</strong>.",
+    NOTIFICATION_ADD_SUCCESS_1 : "The domain <strong>",
+    NOTIFICATION_ADD_SUCCESS_2 : "</strong> has been added successfully.",
+    NOTIFICATION_ADD_FAILED_1 : "Impossible to add the domain <strong>",
+    NOTIFICATION_ADD_FAILED_2 : "</strong>.",
 
-    "NOTIFICATION_EDIT_SUCCESS_1" : "The domain <strong>",
-    "NOTIFICATION_EDIT_SUCCESS_2" : "</strong> has been edited successfully.",
-    "NOTIFICATION_EDIT_FAILED_1" : "Impossible to edit the domain <strong>",
-    "NOTIFICATION_EDIT_FAILED_2" : "</strong>."};
+    NOTIFICATION_EDIT_SUCCESS_1 : "The domain <strong>",
+    NOTIFICATION_EDIT_SUCCESS_2 : "</strong> has been edited successfully.",
+    NOTIFICATION_EDIT_FAILED_1 : "Impossible to edit the domain <strong>",
+    NOTIFICATION_EDIT_FAILED_2 : "</strong>."};
 
-    var chinese = {"NOTIFICATION_WELCOME" : "感谢您使用这个插件。所有的代码是在<a href=\"https://github.com/eayun/UIPlugin-Engine-Manage-Domains\">GitHub</a>上。如果您有任何建议，请在<a href=\"https://github.com/eayun/UIPlugin-Engine-Manage-Domains/issues\">GitHub上提一个issue</a>。",
-    "NOTIFICATION_NEED_RESTART" : "oVirt引擎需要重新启动，以更修改生效（service ovirt-engine restart）。",
-    "NOTIFICATION_REMOVE_USERS" : "请删除所有使用了管理门户或API的域的用户和组。",
+    var chinese = {
+      NOTIFICATION_WELCOME : "感谢您使用这个插件。所有的代码是在<a href=\"https://github.com/eayun/UIPlugin-Engine-Manage-Domains\">GitHub</a>上。如果您有任何建议，请在<a href=\"https://github.com/eayun/UIPlugin-Engine-Manage-Domains/issues\">GitHub上提一个issue</a>。",
+    NOTIFICATION_NEED_RESTART : "oVirt引擎需要重新启动，以更修改生效（service ovirt-engine restart）。",
+    NOTIFICATION_REMOVE_USERS : "请删除所有使用了管理门户或API的域的用户和组。",
 
-    "NOTIFICATION_REFRESH_FAILED" : "无法刷新域的列表。",
-    "NOTIFICATION_REFRESH_SUCCESS" : "域名列表中已成功刷新。",
+    NOTIFICATION_REFRESH_FAILED : "无法刷新域的列表。",
+    NOTIFICATION_REFRESH_SUCCESS : "域名列表中已成功刷新。",
 
-    "NOTIFICATION_DELETE_SUCCESS_1" : "<strong>",
-    "NOTIFICATION_DELETE_SUCCESS_2" : "</strong>已成功删除。",
-    "NOTIFICATION_DELETE_FAILED_1" : "无法删除域<strong>",
-    "NOTIFICATION_DELETE_FAILED_2" : "</strong>。",
+    NOTIFICATION_DELETE_SUCCESS_1 : "<strong>",
+    NOTIFICATION_DELETE_SUCCESS_2 : "</strong>已成功删除。",
+    NOTIFICATION_DELETE_FAILED_1 : "无法删除域<strong>",
+    NOTIFICATION_DELETE_FAILED_2 : "</strong>。",
 
-    "NOTIFICATION_ADD_SUCCESS_1" : "域<strong>",
-    "NOTIFICATION_ADD_SUCCESS_2" : "</strong>已成功添加。",
-    "NOTIFICATION_ADD_FAILED_1" : "无法添加域<strong>",
-    "NOTIFICATION_ADD_FAILED_2" : "</strong>。",
+    NOTIFICATION_ADD_SUCCESS_1 : "域<strong>",
+    NOTIFICATION_ADD_SUCCESS_2 : "</strong>已成功添加。",
+    NOTIFICATION_ADD_FAILED_1 : "无法添加域<strong>",
+    NOTIFICATION_ADD_FAILED_2 : "</strong>。",
 
-    "NOTIFICATION_EDIT_SUCCESS_1" : "域<strong>",
-    "NOTIFICATION_EDIT_SUCCESS_2" : "</strong>已成功编辑。",
-    "NOTIFICATION_EDIT_FAILED_1" : "无法编辑域<strong>",
-    "NOTIFICATION_EDIT_FAILED_2" : "</strong>。"};
+    NOTIFICATION_EDIT_SUCCESS_1 : "域<strong>",
+    NOTIFICATION_EDIT_SUCCESS_2 : "</strong>已成功编辑。",
+    NOTIFICATION_EDIT_FAILED_1 : "无法编辑域<strong>",
+    NOTIFICATION_EDIT_FAILED_2 : "</strong>。"};
   });
 
   return {
