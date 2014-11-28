210d4669b0d648200c4999aba9c1ba0e10a04ec2
Lucas Vandroux <lucas.vandroux@gmail.com>
2014-11-28 11:03:58 +0800

    Finish to translate the notification messages.

--- UIPlugin/emdplugin-resources/js/ajax.js
+++ UIPlugin/emdplugin-resources/js/ajax.js
@@ -8,7 +8,9 @@
   app.value('URL', '/engineManageDomains');
   app.value('sourceName', 'API Controller');
 
-  app.factory('request',['$http', 'URL', 'sourceName', 'messager', 'translateService', function($http, URL, sourceName, messager, tr){
+  app.factory('request',['$http', 'URL', 'sourceName', 'messager', 'translateService', function($http, URL, sourceName, messager, translateService){
+
+    var trans = translateService.get('zh');
 
     return {
 
@@ -20,12 +22,12 @@
         success(function(data, status, headers, config) {
           console.info('List request was successful.(' + status + ')');
           console.log(data);
-          messager.sendDataMessage(sourceName, 'updateDomains', null, tr.get('zh').NOTIFICATION_REFRESH_SUCCESS);
+          messager.sendDataMessage(sourceName, 'updateDomains', null, trans.NOTIFICATION_REFRESH_SUCCESS);
         }).
         error(function(data, status, headers, config) {
           console.error('List request failed.(' + status + ')');
           console.log(data);
-          messager.sendDataMessage(sourceName, 'updateDomainsFailed', null, tr.get('zh').NOTIFICATION_REFRESH_FAILED);
+          messager.sendDataMessage(sourceName, 'updateDomainsFailed', null, trans.NOTIFICATION_REFRESH_FAILED);
         });
       },
 
@@ -35,11 +37,6 @@
 
         var urlReq = URL + '/domains/' + domain2delete;
         console.log('API Request: DELETE - ' + urlReq);
-
-        var trans = tr.get('zh');
-
-        // DEBUG
-        console.log(trans);
 
         var config= {"method": "DELETE"};
 
@@ -52,7 +49,7 @@
         error(function(data, status, headers, config) {
           console.error('Delete request for the domain ' + domain2delete + ' failed.(' + status + ')');
           console.log(data);
-          var failedDeleteText = trans.NOTIFICATION_DELETE_FAILED + '<strong>' + domain2delete + '</strong>.';
+          var failedDeleteText = trans.NOTIFICATION_DELETE_FAILED_1 + '<strong>' + domain2delete + '</strong>' + trans.NOTIFICATION_DELETE_FAILED_2;
           messager.sendDataMessage(sourceName, 'requestFailed', 'remove-dialog', failedDeleteText);
         });
       },
@@ -64,15 +61,14 @@
         success(function(data, status, headers, config) {
           console.info(domain2add.domain + 'has been successfully added.(' + status + ')');
           console.log(data);
-          // replace data by a phrase see translation
-          var successMsg = data + '<ul><li>' + msgRestartEngine + '</li></ul>';
+          var successMsg = trans.NOTIFICATION_ADD_SUCCESS_1 + '<strong>' + domain2add.domain +'</strong>' + NOTIFICATION_ADD_SUCCESS_2 + '<ul><li>' + trans.NOTIFICATION_NEED_RESTART + '</li></ul>';
           messager.sendDataMessage(sourceName, 'requestSuccessful', 'add-dialog', successMsg);
         }).
         error(function(data, status, headers, config) {
           console.error('Add request for the domain ' + domain2add.domain + ' failed.(' + status + ')');
           console.log(data);
-          //replace data by a sentence before putting error message
-          messager.sendDataMessage(sourceName, 'requestFailed', 'add-dialog', data);
+          var failedMsg = trans.NOTIFICATION_ADD_FAILED_1 + '<strong>' + domain2add.domain + '</strong>' + trans.NOTIFICATION_ADD_FAILED_2;
+          messager.sendDataMessage(sourceName, 'requestFailed', 'add-dialog', failedMsg);
         });
       },
 
@@ -84,14 +80,14 @@
           console.info(domain2edit.domain + 'has been successfully edited.(' + status + ')');
           console.log(data);
           //change message edit
-          var successMsg = data + '<ul><li>' + msgRestartEngine + '</li></ul>';
+          var successMsg = trans.NOTIFICATION_EDIT_SUCCESS_1 + '<strong>' + domain2edit.domain + '</strong>' + trans.NOTIFICATION_EDIT_SUCCESS_2 + '<ul><li>' + trans.NOTIFICATION_NEED_RESTART + '</li></ul>';
           messager.sendDataMessage(sourceName, 'requestSuccessful', 'edit-dialog', successMsg);
         }).
         error(function(data, status, headers, config) {
           console.error('Edit request for the domain ' + domain2edit.domain + ' failed.(' + status + ')');
           console.log(data);
-          //change message
-          messager.sendDataMessage(sourceName, 'requestFailed', 'edit-dialog', data);
+          var failedMsg = trans.NOTIFICATION_EDIT_FAILED_1 + '<strong>' + domain2edit.domain + '</strong>' + trans.NOTIFICATION_EDIT_FAILED_2;
+          messager.sendDataMessage(sourceName, 'requestFailed', 'edit-dialog', failedMsg);
         });
       }
     };
@@ -107,18 +103,17 @@
     NOTIFICATION_REFRESH_SUCCESS : "The list of Domains has been refreshed successfully.",
 
     NOTIFICATION_DELETE_SUCCESS : " has been successfully deleted.",
-    NOTIFICATION_DELETE_FAILED_1 : "Impossible to delete the domain <strong>",
-    NOTIFICATION_DELETE_FAILED_2 : "</strong>.",
-
-    NOTIFICATION_ADD_SUCCESS_1 : "The domain <strong>",
-    NOTIFICATION_ADD_SUCCESS_2 : "</strong> has been added successfully.",
+    NOTIFICATION_DELETE_FAILED_1 : "Impossible to delete the domain ",
+    NOTIFICATION_DELETE_FAILED_2 : ".",
+    NOTIFICATION_ADD_SUCCESS_1 : "The domain ",
+    NOTIFICATION_ADD_SUCCESS_2 : " has been added successfully.",
     NOTIFICATION_ADD_FAILED_1 : "Impossible to add the domain <strong>",
     NOTIFICATION_ADD_FAILED_2 : "</strong>.",
 
-    NOTIFICATION_EDIT_SUCCESS_1 : "The domain <strong>",
-    NOTIFICATION_EDIT_SUCCESS_2 : "</strong> has been edited successfully.",
-    NOTIFICATION_EDIT_FAILED_1 : "Impossible to edit the domain <strong>",
-    NOTIFICATION_EDIT_FAILED_2 : "</strong>."};
+    NOTIFICATION_EDIT_SUCCESS_1 : "The domain ",
+    NOTIFICATION_EDIT_SUCCESS_2 : " has been edited successfully.",
+    NOTIFICATION_EDIT_FAILED_1 : "Impossible to edit the domain ",
+    NOTIFICATION_EDIT_FAILED_2 : "."};
 
     var chinese = {
       NOTIFICATION_WELCOME : "感谢您使用这个插件。所有的代码是在<a href=\"https://github.com/eayun/UIPlugin-Engine-Manage-Domains\">GitHub</a>上。如果您有任何建议，请在<a href=\"https://github.com/eayun/UIPlugin-Engine-Manage-Domains/issues\">GitHub上提一个issue</a>。",
@@ -129,18 +124,18 @@
     NOTIFICATION_REFRESH_SUCCESS : "域名列表中已成功刷新。",
 
     NOTIFICATION_DELETE_SUCCESS : "已成功删除。",
-    NOTIFICATION_DELETE_FAILED_1 : "无法删除域<strong>",
-    NOTIFICATION_DELETE_FAILED_2 : "</strong>。",
+    NOTIFICATION_DELETE_FAILED_1 : "无法删除域",
+    NOTIFICATION_DELETE_FAILED_2 : "。",
 
-    NOTIFICATION_ADD_SUCCESS_1 : "域<strong>",
-    NOTIFICATION_ADD_SUCCESS_2 : "</strong>已成功添加。",
+    NOTIFICATION_ADD_SUCCESS_1 : "域",
+    NOTIFICATION_ADD_SUCCESS_2 : "已成功添加。",
     NOTIFICATION_ADD_FAILED_1 : "无法添加域<strong>",
     NOTIFICATION_ADD_FAILED_2 : "</strong>。",
 
-    NOTIFICATION_EDIT_SUCCESS_1 : "域<strong>",
-    NOTIFICATION_EDIT_SUCCESS_2 : "</strong>已成功编辑。",
-    NOTIFICATION_EDIT_FAILED_1 : "无法编辑域<strong>",
-    NOTIFICATION_EDIT_FAILED_2 : "</strong>。"};
+    NOTIFICATION_EDIT_SUCCESS_1 : "域",
+    NOTIFICATION_EDIT_SUCCESS_2 : "已成功编辑。",
+    NOTIFICATION_EDIT_FAILED_1 : "无法编辑域",
+    NOTIFICATION_EDIT_FAILED_2 : "。"};
 
   return {
     get: function (langKey) {
